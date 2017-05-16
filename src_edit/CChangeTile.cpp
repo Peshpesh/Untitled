a7ebf44cfad72ab34e9a8f28547a09d3a43a2594
Sephimink <sephimink@live.com>
2017-05-16 03:11:28 -0400

    Pick tile arrows functionality

--- src_edit/CChangeTile.cpp
+++ src_edit/CChangeTile.cpp
@@ -37,7 +37,38 @@ bool CChangeTile::OnLClick(int mX, int mY, int& ID)
 	else
 	{
 		// Maybe an arrow was clicked?
+		int aX, aY;
+		if (W > MAX_TILES)	// Left/right arrows are worth processing
+		{
+			aY = dispY + (dispH / 2) - (ARROW_SIZE / 2);
+			if (X > 0) 							// Left arrow?
+			{
+				aX = dispX - (MENU_BORDER_SIZ + ARROW_SIZE + SYM_SPACING);
+				if (mX >= aX && mX < aX + ARROW_SIZE && mY >= aY && mY < aY + ARROW_SIZE)	X--;
+			}
 
+			if (X < W - MAX_TILES)	// Right arrow?
+			{
+				aX = dispX + dispW + MENU_BORDER_SIZ + SYM_SPACING;
+				if (mX >= aX && mX < aX + ARROW_SIZE && mY >= aY && mY < aY + ARROW_SIZE)	X++;
+			}
+		}
+
+		if (H > MAX_TILES)	// Up/down arrows are worth processing
+		{
+			aX = dispX + (dispW / 2) - (ARROW_SIZE / 2);
+			if (Y > 0) 							// Up arrow?
+			{
+				aY = dispY - (MENU_BORDER_SIZ + ARROW_SIZE + SYM_SPACING);
+				if (mX >= aX && mX < aX + ARROW_SIZE && mY >= aY && mY < aY + ARROW_SIZE)	Y--;
+			}
+
+			if (Y < H - MAX_TILES)	// Down arrow?
+			{
+				aY = dispY + dispH + MENU_BORDER_SIZ + SYM_SPACING;
+				if (mX >= aX && mX < aX + ARROW_SIZE && mY >= aY && mY < aY + ARROW_SIZE)	Y++;
+			}
+		}
 	}
 	return retval;
 }
@@ -49,8 +80,7 @@ bool CChangeTile::RenderTileset(SDL_Renderer* renderer, SDL_Texture* interface, 
 	CSurface::OnDraw(renderer, interface, dispX - MENU_BORDER_SIZ, dispY - MENU_BORDER_SIZ,
 		LIGHTS_X, COLOR_PURE_Y, 1, 1, dispW + (MENU_BORDER_SIZ*2), dispH + (MENU_BORDER_SIZ*2));
 	// Render the tileset (or part of it)
-	CSurface::OnDraw(renderer, tileset, dispX, dispY, X, Y, dispW, dispH);
-
+	CSurface::OnDraw(renderer, tileset, dispX, dispY, X * TILE_SIZE, Y * TILE_SIZE, dispW, dispH);
 
 	// Render clickable arrows
 	aX = dispX - (MENU_BORDER_SIZ + ARROW_SIZE + SYM_SPACING);
@@ -64,7 +94,6 @@ bool CChangeTile::RenderTileset(SDL_Renderer* renderer, SDL_Texture* interface, 
 	CSurface::OnDraw(renderer, interface, aX, aY, U_ARROW_XO, U_ARROW_YO, ARROW_SIZE, ARROW_SIZE);
 	aY = dispY + dispH + MENU_BORDER_SIZ + SYM_SPACING;
 	CSurface::OnDraw(renderer, interface, aX, aY, D_ARROW_XO, D_ARROW_YO, ARROW_SIZE, ARROW_SIZE);
-
 
 	return true;
 }
